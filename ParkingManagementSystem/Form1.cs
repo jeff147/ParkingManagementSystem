334b2a5793a5360d4b5ffa7ee581a9effe171a71
jeff147 <jre5791@gmail.com>
2017-10-24 22:33:55 -0400

    Setup, imports, and such.

--- ParkingManagementSystem/Form1.cs
+++ ParkingManagementSystem/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace ParkingManagementSystem
 {
@@ -15,6 +17,11 @@ namespace ParkingManagementSystem
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
