3f5a0b118ac7ed75b5648d3f3784025dd0f40efd
jeff147 <jre5791@gmail.com>
2017-12-15 13:11:55 -0500

    More database stuff

--- ParkingManagementSystem/issuePermit.cs
+++ ParkingManagementSystem/issuePermit.cs
@@ -7,18 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
 
 namespace ParkingManagementSystem
 {
     public partial class issuePermit : Form
     {
+        SqlConnection connection;
+        string connectionString;
+
         public issuePermit()
         {
             InitializeComponent();
+
+            connectionString = ConfigurationManager.ConnectionStrings["ParkingManagementSystem.Properties.Settings.ParkingDatabaseConnectionString"].ConnectionString;
         }
 
         private void btnIssuePermit_Click(object sender, EventArgs e)
         {
+            string query = "SET IDENTITY_INSERT Vehicles ON;INSERT INTO Vehicles(vehicleId, licensePlate, state, make, model, year, color, VIN) " +
+                "VALUES ('@vehicleId', '@licensePlate', '@state', '@make', '@model', '@year', '@color', '@VIN');SET IDENTITY_INSERT Vehicles OFF;";
+
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            //using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                connection.Open();
+
+                command.Parameters.AddWithValue("@permit", Int32.Parse(vehicleIdBox.Text));
+                command.Parameters.AddWithValue("@licensePlate", licensePlateBox.Text);
+                command.Parameters.AddWithValue("@state", stateBox.Text);
+                command.Parameters.AddWithValue("@make", makeBox.Text);
+                command.Parameters.AddWithValue("@model", modelBox.Text);
+                command.Parameters.AddWithValue("@year", yearBox.Text);
+                command.Parameters.AddWithValue("@color", colorBox.Text);
+                command.Parameters.AddWithValue("@VIN", vinBox.Text);
+
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
             this.Hide();
         }
     }
